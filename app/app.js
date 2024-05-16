d6b23a688a22b64fb97ca3a400e55319e867b3f0
Matheus Tavares <teteupower@gmail.com>
2024-05-16 00:55:27 -0300

    Adicionando animações nas trocas de página, redefinindo as páginas, criando lista de clientes, criando botões de opções para os clientes

--- app/app.js
+++ app/app.js
@@ -33,3 +33,50 @@ window.addEventListener('pageshow', (event) => {
     }, { once: true });
 });
 
+// Lógica para mostrar/esconder os formulários e preencher o nome do cliente
+const listaClientes = document.getElementById('listaClientes');
+const opcoesCliente = document.getElementById('opcoesCliente');
+const clienteInput = document.getElementById('cliente'); // Input de pesquisa
+
+listaClientes.addEventListener('click', (event) => {
+    const clienteId = event.target.dataset.clienteId;
+    const nomeCliente = event.target.textContent;
+
+    // Exibe as opções do cliente APENAS se um item da lista for clicado
+    if (event.target.tagName === 'LI') { 
+        opcoesCliente.style.display = 'block';
+        document.querySelectorAll('#formPagamento h3, #formEmprestimo h3').forEach(h3 => {
+            h3.textContent = `${h3.textContent} - ${nomeCliente}`;
+        });
+        clienteInput.value = nomeCliente;
+    }
+});
+
+// Adiciona um evento de input ao campo de pesquisa
+clienteInput.addEventListener('input', () => {
+    const termoPesquisa = clienteInput.value.toLowerCase();
+    const itensLista = listaClientes.querySelectorAll('li');
+
+    itensLista.forEach(item => {
+        const nomeCliente = item.textContent.toLowerCase();
+        item.style.display = nomeCliente.includes(termoPesquisa) ? 'block' : 'none';
+    });
+});
+
+// Adiciona um evento de clique ao botão "Limpar"
+document.getElementById('btnLimparPesquisa').addEventListener('click', () => {
+    clienteInput.value = ''; // Limpa o input de pesquisa
+
+    // Exibe todos os itens da lista
+    const itensLista = listaClientes.querySelectorAll('li');
+    itensLista.forEach(item => {
+        item.style.display = 'block'; 
+    });
+
+    // Esconde as opções do cliente
+    opcoesCliente.style.display = 'none';
+
+    document.querySelectorAll('#formPagamento h3, #formEmprestimo h3').forEach(h3 => {
+        h3.textContent = h3.textContent.split(' - ')[0]; // Remove o nome do cliente dos títulos
+    });
+});
